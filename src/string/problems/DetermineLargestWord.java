2d424fde1be9d89933c8aafe1cce59e612f3d93e
imohammed <iliasmohammed0@gmail.com>
2018-12-24 20:25:17 -0500

    Midterm String Problems

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -13,14 +13,26 @@ public class DetermineLargestWord {
         String s="Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
-
+        int max=0;
+        String maxWord="";
+        for(Map.Entry<Integer,String>wordLength:wordNLength.entrySet()){
+            if(max<wordLength.getKey()){
+                max = wordLength.getKey();
+                maxWord=wordLength.getValue();
+            }
+        }
+        System.out.println("Longest word: "+maxWord);
+        System.out.println("Length of word: "+max);
     }
 
     public static Map<Integer, String> findTheLargestWord(String wordGiven){
         Map<Integer, String> map = new HashMap<Integer, String>();
         String st = "";
         //implement
-
+        String[] array=wordGiven.split(" ");
+        for(int a=0;+a<array.length;a++ ){
+            map.put(array[a].length(), array[a]);
+        }
         return map;
     }
 }
