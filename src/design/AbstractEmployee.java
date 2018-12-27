82bed65e3c081b188146ec60517c1403f82e3c18
imohammed <iliasmohammed0@gmail.com>
2018-12-26 19:49:26 -0500

    Update

--- src/design/AbstractEmployee.java
+++ src/design/AbstractEmployee.java
@@ -1,5 +1,29 @@
 package design;
 
 public abstract class AbstractEmployee implements Employee{
+    int employeeID;
+    String emplyoeeId;
+    String assignDepartment;
+    int calculateSalary;
+    String benefitLayout;
 
+    public int employeeId() {
+        return 0;
+    }
+
+    public String employeeName() {
+        return null;
+    }
+
+    public void assignDepartment() {
+
+    }
+
+    public int calculateSalary() {
+        return 0;
+    }
+
+    public void benefitLayout() {
+
+    }
 }
