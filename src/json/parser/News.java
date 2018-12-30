c9a96504c6bb781863b42913f5cfc0c12c3db9d7
imohammed <iliasmohammed0@gmail.com>
2018-12-30 11:55:20 -0500

    update

--- src/json/parser/News.java
+++ src/json/parser/News.java
@@ -1,26 +1,68 @@
 package json.parser;
 
 public class News {
-
-    private String article;
+    private String source;
+    private String url;
+    private String publishedAt;
+    private String content;
+    private String urlToImage;
     private String author;
     private String description;
     private String title;
 
-    public News() {}
+    public News() {
+    }
 
-    public News(String article, String author, String description, String title) {
-        this.article = article;
+    public News(String content, String author, String description, String title,
+                String source, String url, String publishedAt, String urlToImage) {
+        this.urlToImage = urlToImage;
         this.author = author;
         this.description = description;
         this.title = title;
+        this.source = source;
+        this.url = url;
+        this.publishedAt = publishedAt;
+        this.content = content;
     }
 
-        public String getArticle() {
-            return article; }
+        public String getSource () {
+            return source;
+        }
 
-        public void setArticle (String article){
-            this.article = article;
+        public void setSource (String source){
+            this.source = source;
+        }
+
+        public String getUrl () {
+            return url;
+        }
+
+        public void setUrl (String url){
+            this.url = url;
+        }
+
+        public String getPublishedAt () {
+            return publishedAt;
+        }
+
+        public void setPublishedAt (String publishedAt){
+            this.publishedAt = publishedAt;
+        }
+
+        public String getContent () {
+            return content;
+        }
+
+        public void setContent (String content){
+            this.content = content;
+        }
+
+        public String getUrlToImage () {
+            return urlToImage;
+        }
+
+        public void setUrlToImage (String urlToImage){
+            this.urlToImage = urlToImage;
         }
 
         public String getAuthor () {
@@ -46,4 +88,5 @@ public class News {
         public void setTitle (String title){
             this.title = title;
         }
+
 }
